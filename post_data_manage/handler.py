1b28ce74ac5458de2d9dc9182e8364cb456e1898
TongYang <springqaq@gmail.com>
2021-12-23 23:36:13 -0800

    add task status update

--- post_data_manage/handler.py
+++ post_data_manage/handler.py
@@ -1,4 +1,5 @@
 from data.mongo import convert, save_to_post_predict
+from mysql.update_task import update_task
 from process.pro_process import pre_process
 from process.process import create_sample
 import os
@@ -6,6 +7,7 @@ import sys
 import time
 import tqdm
 import shutil
+
 
 def write_to_file(path, data):
     if os.path.exists(path):
@@ -15,22 +17,28 @@ def write_to_file(path, data):
             line = item['raw']
             file.write(line+'\n')
             pass
-ws = os.path.dirname(__file__) +'/ws/'
-def do_task(task_tag):
-    [region,date] = task_tag.split('-')
+
+
+ws = os.path.dirname(__file__) + '/ws/'
+
+
+def do_task(task_tag, task_id):
+    [region, date] = task_tag.split('-')
     uuid = task_tag
-    fin= ws +'/raw_data/'+uuid+'.csv'
-    fin_temp = temp_fout=ws + '/temp/' + uuid +'/'
-    
+    fin = ws + '/raw_data/'+uuid+'.csv'
+    fin_temp = temp_fout = ws + '/temp/' + uuid + '/'
+    update_task(uuid, 2) 
     write_to_file(fin, convert(task_tag))
-    pre_process(fin=fin,fout=temp_fout, is_test=False)#
-    last_x, last_len, global_x, unpick_x, unpick_len, unpick_geo, days_np, order_np, index_np, eta_np, dic_geo2index = create_sample(fin_temp=fin_temp, day_window=(50,60), block={0},
-                                        label_range=(0, 120), len_range=(1, 25), graph_ret=False)
+    pre_process(fin=fin, fout=temp_fout, is_test=False)
+    update_task(uuid, 3)
+    last_x, last_len, global_x, unpick_x, unpick_len, unpick_geo, days_np, order_np, index_np, eta_np, dic_geo2index = create_sample(fin_temp=fin_temp, day_window=(50, 60), block={0},
+                                                                                                                                     label_range=(0, 120), len_range=(1, 25), graph_ret=False)
+    update_task(uuid, 4)
     os.remove(fin)
     shutil.rmtree(fin_temp)
-    save_to_post_predict(region,date,unpick_x,unpick_len,order_np)
-    print(len(unpick_x))   
-
+    save_to_post_predict(region, date, unpick_x, unpick_len, order_np)
+    update_task(uuid, 5)
+    print(len(unpick_x))
 
 
 ''''
@@ -40,12 +48,14 @@ def do_task(task_tag):
     "task_tag":"xxx"
 }
 '''
+
+
 def handler_msg(msg):
-    print("[Got] :",msg)
+    print("[Got] :", msg)
     body = msg.body
-    import json 
+    import json
     task = json.loads(body)
     task_id = task['task_id']
     task_tag = task['task_tag']
-    do_task(task_tag)
-    print("[FINISH] :",msg)
+    do_task(task_tag, task_id)
+    print("[FINISH] :", msg)
